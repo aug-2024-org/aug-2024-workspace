7faa60d05dbc51d59618f6835cd93daa9b99b0dd
bpoongs <bpoongs@gmail.com>
2024-08-12 14:37:24 +0530

    demo

---
+++ core-java-demo/src/OperatosDemo.java
@@ -0,0 +1,35 @@
+
+public class OperatosDemo {
+	// Arithmetic operators - + - * / %
+	// Logical Operators - &&  ||  !
+	// Relational Operator - < <= > >= != ==
+	// Assignment Operator - =
+	// Arithmetic Assignment Operator - += -= /= *= %=
+	// Bitwise Operators - & | ! << >> >>>
+	
+	// Unary Operators - + - ++ --
+	// Binary Operators 
+	// Ternary Operators - ?:
+	
+	public static void main(String[] args) {
+		
+		int num1 = 50;
+		int num2 = 60;
+		int largestNum = (num1>num2)?num1:num2;
+		System.out.println("Largest of " + num1 + " and " + num2 + " is " + largestNum);
+
+		String result = (num1%2==0)?"Even":"Odd";
+		System.out.println(num1 + " is " + result);
+		
+		int result1 = 5>>2;
+		System.out.println(result1);
+		
+		int result2 = 5<<2;
+		System.out.println(result2);
+		
+		int result3 = 5&3;
+		System.out.println(result3);
+		
+	}
+
+}
